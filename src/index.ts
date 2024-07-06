19490fb4b3c652a49e1d5ab4fd0af6cf0520f864
AnushH18 <anushrh@gmail.com>
2024-07-06 22:36:39 +0530

    created a basic server

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, {Express} from "express";
+import serverConfig from "./config/serverConfig";
+
+const app : Express = express();
+
+app.listen(serverConfig.PORT , ()=>{
+    console.log(`Server started in port ${serverConfig.PORT} `);
+    
+});
