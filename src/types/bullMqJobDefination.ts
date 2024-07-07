972764d19d0c11464c2f91050ad644e29f1cfd29
AnushH18 <anushrh@gmail.com>
2024-07-07 14:52:19 +0530

    added sample job and queue

---
+++ src/types/bullMqJobDefination.ts
@@ -0,0 +1,9 @@
+import { Job } from "bullmq";
+
+export interface IJob{
+    name : string
+    payload? : Record<string, unknown>
+    handle : (job? : Job) => void
+    failed : (ob? : Job) => void
+}
+
